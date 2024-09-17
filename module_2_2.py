54486465fd82ded51ec4e337cb1d3258599c5077
DjonHd <djonohara@gmail.com>
2024-09-17 15:42:46 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,13 @@
+first = 123
+second = 345
+third = 789
+if first == second == third:
+    print('3')
+if first != second != third:
+    print('0')
+first = 42
+second = 69
+third = 42
+if (first == second or first == third or secod == third) and not first == second == third:
+    print('2')
+
